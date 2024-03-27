df383568cec54db8d64b422edef3189978577fe2
Skull Valley RP <165104468+SkullValleyRP@users.noreply.github.com>
2024-03-26 22:01:41 -0400

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,12 @@
 fx_version "adamant"
 game "rdr3"
-author 'SkullValleyRP'
-description 'This places 2 desks and 1 set of bunk beds on the first floor, and 4 sets of bunk beds on the second floor of the Green House in Valentine.'
+lua54 'yes'
+
+this_is_a_map "yes"
 
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 
-dependency 'objectloader'
-
 files {
-	'recruit_bay.xml'
-}
+  "stream/tabak-val.ymap",
 
-objectloader_maps {
-	'recruit_bay.xml'
 }
