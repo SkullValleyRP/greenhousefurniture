7739f3b60611083fb77a10c0f9bace6a831557f4
SkullValleyRP <165104468+SkullValleyRP@users.noreply.github.com>
2024-03-26 20:42:53 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "adamant"
+game "rdr3"
+author 'SkullValleyRP'
+description 'This places 2 desks and 1 set of bunk beds on the first floor, and 4 sets of bunk beds on the second floor of the Green House in Valentine.'
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+
+dependency 'objectloader'
+
+files {
+	'recruit_bay.xml'
+}
+
+objectloader_maps {
+	'recruit_bay.xml'
+}
